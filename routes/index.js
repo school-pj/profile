9d57cb284cef86359e6e573c69b4af7d032d2efd
yamamoto_shunsuke <s.yamamoto@atomitech.jp>
2020-11-10 16:15:12 +0900

    自己紹介文が更新されない部分を修正

--- routes/index.js
+++ routes/index.js
@@ -18,42 +18,25 @@ var knex = require('knex')({
 router.get('/', function (req, res, next) {
   if (req.session.user_name) {
     knex
-    .select()
-    .from('users')
-    .then(function(rows) {
-      res.render('index', { title: 'ProfileApp' , user_name: req.session.user_name , password: req.session.password, contentList: rows});
-  })
-    .catch(function(error) {
-    console.error(error)
-  });
-   }else{
-    res.render('index', { title: 'Welcome to ProfileApp' , user_name: req.session.user_name ,password: req.session.password});
-   }
-});
-
-router.post("/", (req,res,next) => {
-  const user_name = req.session.user_name;
-  const password = req.session.password;
-  const content = req.body.content;
-
-    knex.insert({ user_name,password,content: user_name,password,content })
-      .into('users')
+      .select()
+      .from('users')
       .then(function (rows) {
-        res.redirect("/");
+        res.render('index', { title: 'ProfileApp', user_name: req.session.user_name, password: req.session.password, contentList: rows});
       })
       .catch(function (error) {
-        console.error(error);
-
-        res.redirect("/");
+        console.error(error)
       });
+  } else {
+    res.render('index', { title: 'Welcome to ProfileApp', user_name: req.session.user_name, password: req.session.password });
+  }
 });
 
 router.post("/", (req, res, next) => {
   const user_name = req.session.user_name;
   const password = req.session.password;
   const content = req.body.content;
-  if(content != null){
-    knex('users')
+
+  knex('users')
     .where({ user_name, password: user_name, password })
     .update({ content: content })
     .then(function (rows) {
@@ -64,9 +47,6 @@ router.post("/", (req, res, next) => {
 
       res.redirect("/");
     });
-  }else{
-    res.redirect("/");
-  }
 
 });
 
