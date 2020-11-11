6cc96d23ca7d76c6d185bef4ba82e71b98f09a14
hikeshita <h.ikeshita@atomitech.jp>
2020-11-11 11:51:06 +0900

    ユーザー登録ページ_設定ページの機能追加

--- routes/login.js
+++ routes/login.js
@@ -1,77 +1,78 @@
 var passport = require("passport");
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 var LocalStrategy = require("passport-local").Strategy;
 var initialize, authenticate, authorize;
-var knex = require('knex')({
-  client: 'mysql',
+var knex = require("knex")({
+  client: "mysql",
   connection: {
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'profileapp'
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "profileapp",
   },
-  useNullAsDefault: true
+  useNullAsDefault: true,
 });
-
 
 //サーバからクライアントに保存する処理
-passport.serializeUser((user_name,done) => {
-  done(null,user_name); 
+passport.serializeUser((user_name, done) => {
+  done(null, user_name);
 });
 
-
 //クライアントからサーバに復元する処理
-passport.deserializeUser((user_name,done) => {
-  done(null,user_name);
+passport.deserializeUser((user_name, done) => {
+  done(null, user_name);
 });
 
 //ユーザー名とパスワードを利用した認証
-passport.use("local-strategy",
-  new LocalStrategy({
-    usernameField: "username", 
-    passwordField: "password", 
-    passReqToCallback: true
-  }, (req, user_name, password, done) => { 
-
-    knex("users")
-      .where({user_name, password: user_name, password })
-      .then(function (rows) {
-        //成功
-        if (rows.length !== 0) {
-          req.session.user_name = user_name;
-          req.session.user_id = rows[0].id;
-          req.session.password = password;
-          done(null,user_name);
-        } else {
-          done(null, false, req.flash("message", "ユーザー名 または パスワード が間違っています。"));
-        }
-      });
-  }));
+passport.use(
+  "local-strategy",
+  new LocalStrategy(
+    {
+      usernameField: "username",
+      passwordField: "password",
+      passReqToCallback: true,
+    },
+    (req, user_name, password, done) => {
+      knex("users")
+        .where({ user_name, password: user_name, password })
+        .then(function (rows) {
+          //成功
+          if (rows.length !== 0) {
+            req.session.user_name = user_name;
+            req.session.user_id = rows[0].id;
+            req.session.password = password;
+            done(null, user_name);
+          } else {
+            done(
+              null,
+              false,
+              req.flash(
+                "message",
+                "ユーザー名 または パスワード が間違っています。"
+              )
+            );
+          }
+        });
+    }
+  )
+);
 
 initialize = function () {
-  return [
-    passport.initialize(),
-    passport.session()
-  ];
+  return [passport.initialize(), passport.session()];
 };
 
 //認証成功時処理
 authenticate = function () {
-  return passport.authenticate(
-    "local-strategy", {
+  return passport.authenticate("local-strategy", {
     successRedirect: "/",
-    failureRedirect: "/login"
-  }
-  );
+    failureRedirect: "/login",
+  });
 };
-
-
-
 
 module.exports = {
   initialize,
   authenticate,
   authorize,
-  router
+  router,
 };
--- routes/setting.js
+++ routes/setting.js
@@ -1,56 +1,50 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-
-var knex = require('knex')({
-  client: 'mysql',
+var knex = require("knex")({
+  client: "mysql",
   connection: {
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'profileapp'
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "profileapp",
   },
-  useNullAsDefault: true
+  useNullAsDefault: true,
 });
 
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
   if (req.session.id) {
-  res.render('setting', {message: req.flash("message") , title: 'アカウント設定' ,user_name:req.session.user_name});
-}
-  else {
-        res.redirect("login");
+    res.render("setting", {
+      message: req.flash("message"),
+      title: "アカウント設定",
+      user_name: req.session.user_name,
+    });
+  } else {
+    res.redirect("login");
   }
 });
 
-
-router.post('/', function(req, res, next) {
+router.post("/", function (req, res, next) {
   var username = req.body.username;
   var password = req.body.password;
   var confirm = req.body.confirm;
   var user_id = req.session.user_id;
 
-    //バリデート処理
-    //TODO:フラッシュメッセージが画面に出るように直す
-  if(password !== confirm){
-    req.flash("message",'パスワードが一致しません'),
-    console.log(req.flash("message"));
-    res.render('setting',{
-        title: "アカウント設定",
-        message: req.flash("message"),
-        user_name: username
-    })
-    return;
-  }
-
-  knex('users')
-      .where({id:user_id})
-      .update({user_name:username,password:password})
+  //バリデート処理
+  if (password !== confirm) {
+    req.flash("message", "パスワードが一致しません");
+    res.redirect("/setting");
+  } else {
+    knex("users")
+      .where({ id: user_id })
+      .update({ user_name: username, password: password })
       .then(function (rows) {
-      res.redirect('/logout');
-      console.log(rows[0]);
-    })
-    .catch(function (error) {
-      console.error(error)
-    });
+        res.redirect("/logout");
+        console.log(rows[0]);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
 });
 module.exports = router;
