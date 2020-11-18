78ecc326b8175b9dee7a2eb1600b8d73035a55d4
hikeshita <h.ikeshita@atomitech.jp>
2020-11-18 12:18:20 +0900

    フォロー＿フォロー解除ボタンを追加

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var { authenticate } = require("./login");
-
 var knex = require('knex')({
   client: 'mysql',
   connection: {
@@ -12,41 +11,48 @@ var knex = require('knex')({
   },
   useNullAsDefault: true
 });
-
-
 /* GET home page. */
+//初期の状態では、relationshipsのほうには値が何も入っていないため、
+//usersテーブル情報を何も取ってこれない状態になっている。
+//そのため、内部結合で初期の状態でもusersテーブルの情報も持ってくるように実装が必要。
 router.get('/', function (req, res, next) {
-  //フォロー数、フォロワー数を表示するため、内部結合を行うように修正を行う。
-  //knex.js　内部結合のsql文
-  //select * from users inner join relationships on users.id = relationships.id
-  //例：knex.from('users').innerJoin('accounts', 'users.id', 'accounts.user_id')
   if (req.session.user_name) {
-    // knex
-    //   .select()
-    //   .from('users')
-      knex
+    knex
       .from('users')
-      .innerJoin('relationships','users.id','relationships.id')
+      .innerJoin('relationships', 'users.id', 'relationships.id')
       .then(function (rows) {
-        req.session.followed_id = rows[0].followed_id;
-        req.session.following_id = rows[0].following_id;
-        res.render('index', { title: 'ProfileApp', user_name: req.session.user_name, contentList: rows, user_id: req.session.user_id, followed_id: req.session.followed_id, following_id: req.session.following_id});
+        if (rows[req.session.user_id - 1] === undefined) {
+          const user_name = req.session.user_name;
+          const user_id = req.session.user_id;
+          //フォローIDとフォロワーIDには0を入れて、他のカラム値情報を持ってくる。
+          knex
+            .from('users')
+            .then(function (rows) {
+              console.log("処理通過！");
+              req.session.followed_id = 0;
+              req.session.following_id = 0;
+              res.render('index', { title: 'ProfileApp', user_name: req.session.user_name, contentList: rows, user_id: req.session.user_id, followed_id: req.session.followed_id, following_id: req.session.following_id });
+            });
+        } else {
+          console.log(rows[0]);
+          req.session.followed_id = rows[req.session.user_id - 1].followed_id;
+          req.session.following_id = rows[req.session.user_id - 1].following_id;
+          res.render('index', { title: 'ProfileApp', user_name: req.session.user_name, contentList: rows, user_id: req.session.user_id, followed_id: req.session.followed_id, following_id: req.session.following_id });
+        }
       })
       .catch(function (error) {
         console.error(error)
       });
   } else {
-    res.render('index', { title: 'Welcome to ProfileApp', user_name: req.session.user_name,user_id: req.session.user_id });
+    res.render('index', { title: 'Welcome to ProfileApp', user_name: req.session.user_name, user_id: req.session.user_id });
   }
 });
-
 router.post("/", (req, res, next) => {
   const user_name = req.session.user_name;
-  const user_id =  req.session.user_id;
+  const user_id = req.session.user_id;
   const content = req.body.content;
-
   knex('users')
-    .where({ id: user_id,user_name: user_name})
+    .where({ id: user_id, user_name: user_name })
     .update({ content: content })
     .then(function (rows) {
       res.redirect("/");
@@ -55,17 +61,12 @@ router.post("/", (req, res, next) => {
       console.error(error);
       res.redirect("/");
     });
-
 });
-
-
 //ログイン処理
 router.get("/login", (req, res, next) => {
-  res.render("login", { message: req.flash("message"), user_name: req.session.user_name});
+  res.render("login", { message: req.flash("message"), user_name: req.session.user_name });
 });
-
 router.post("/login", authenticate());
-
 //ログアウト処理
 router.get("/logout", function (req, res, next) {
   req.session.destroy(function (err) {
@@ -76,5 +77,4 @@ router.get("/logout", function (req, res, next) {
     }
   });
 });
-
 module.exports = router;
--- routes/profile.js
+++ routes/profile.js
@@ -1,40 +1,52 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var knex = require('knex')({
-  client: 'mysql',
+var knex = require("knex")({
+  client: "mysql",
   connection: {
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'profileapp'
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "profileapp",
   },
-  useNullAsDefault: true
+  useNullAsDefault: true,
 });
 
 //View My Profileを押下時の処理
-//TODO : 下の行の:user_idは何を意味するか確認する←user_idという変数の値を持ってきている？？
-router.get('/:user_id',
-  function (req, res, next) {
-    knex
-      .select()
-      .from('users')
-      .then(function (rows) {
-        res.render('profile', {
-          user_name: req.session.user_name,
-          contentList: rows,
-          user_id: req.session.user_id
-        });
-      })
-      .catch(function (error) {
-        console.error(error)
+router.get("/:user_id", function (req, res, next) {
+  req.session.array_id = [];
+  var location = req.url.slice(-1);
+  console.log(req.session.user_id);
+  console.log(location);
+  console.log(req.session.user_id != location);
+  knex
+    .from("users")
+    .innerJoin("relationships", "users.id", "relationships.followed_id")
+    .then(function (rows) {
+      for (var i = 0; i < rows.length; i++) {
+        req.session.array_id[i] = rows[i].following_id;
+      }
+    });
+  knex
+    .select()
+    .from("users")
+    .then(function (rows) {
+      res.render("profile", {
+        user_name: req.session.user_name,
+        contentList: rows,
+        user_id: req.session.user_id,
+        isotherspage: req.session.user_id != location,
+        isfollow: req.session.array_id.includes(location),
       });
-  });
+    })
 
+    .catch(function (error) {
+      console.error(error);
+    });
+});
 
 //View My Profileを押下時の処理
-router.post('/:user_id', function (req, res, next) {
+router.post("/:user_id", function (req, res, next) {
   res.render("profile");
 });
-
 
 module.exports = router;
