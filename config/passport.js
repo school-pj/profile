73f95965f2ad56fa6bb65f93d710a5399747920f
yamamoto-shunsuke <s.yamamoto@atomitech.jp>
2020-12-23 16:47:17 +0900

    passport機能実装の変更

--- config/passport.js
+++ config/passport.js
@@ -30,35 +30,20 @@ module.exports = function (app) {
       {
         usernameField: "username",
         passwordField: "password",
-        passReqToCallback: true,
       },
       (req, user_name, password, done) => {
         knex("users")
           .where({ user_name: user_name })
           .then(async function (rows) {
-            if (rows != "") {
+            if (rows.length != 0) {
               const comparedPassword = await bcrypt.compare(password, rows[0].password);
               if (comparedPassword) {
                 done(null, rows[0]);
               } else {
-                done(
-                  null,
-                  false,
-                  req.flash(
-                    "message",
-                    "The user name or password is incorrect。"
-                  )
-                )
+                return done(null, false, req.flash("message", "The user name or password is incorrect."));
               };
             } else {
-              done(
-                null,
-                false,
-                req.flash(
-                  "message",
-                  "The user name or password is incorrect."
-                )
-              );
+              return done(null, false, req.flash("message", "The user name or password is incorrect."));
             }
           });
       }
@@ -68,15 +53,13 @@ module.exports = function (app) {
   //サーバからクライアントに保存する処理
   //ログイン時(strategy実行時にしか実行されない)
   passport.serializeUser((user, done) => {
-    console.log("sirialize");
     done(null, user.id);
   });
 
 
   //クライアントからサーバに復元する処理
   //セッションの有効期間中は常に機能する。req.userに値を入れる。
-  passport.deserializeUser( async function (id, done) {
-    console.log("deserialize");
+  passport.deserializeUser(async function (id, done) {
     try {
       const user = await User.findById(id);
       done(null, user);
